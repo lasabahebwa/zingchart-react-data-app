06f0448e86b92b459d206dc81da668772d3da7e2
Livingstone Asabahebwa <lasabahebwa@zingsoft.com>
2024-10-25 22:23:35 +0300

    basic zing with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import "zingchart/es6";
+import ZingChart from "zingchart-react";
+import "./App.css";
+
+let chartConfig = {
+  //chart styling
+  type: "line",
+  series: [
+    {
+      values: [11, 26, 7, 44, 19],
+    },
+  ],
+};
 
 function App() {
+  const [config, setConfig] = useState(chartConfig);
+
+  let chart = useRef(null);
+
+  let randomPlot = [];
+
+  function generateRandomPlot(min, max) {
+    for (let i = 0; i < 5; i++) {
+      randomPlot.push(getRandomInt(min, max));
+    }
+  }
+
+  function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  }
+
+  function updateConfig() {
+    generateRandomPlot(0, 100);
+    setConfig({
+      type: "bar",
+      series: [
+        {
+          values: randomPlot,
+        },
+      ],
+    });
+  }
+
+  function reload() {
+    chart.current.reload();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ZingChart data={config} ref={chart} />
+      <div className="controls--container">
+        <button id="demo" onClick={() => updateConfig()}>
+          update
+        </button>
+        <button id="reload" onClick={() => reload()}>
+          reload
+        </button>
+      </div>
     </div>
   );
 }
